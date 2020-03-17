5d758e38f1e585e2c95711085c87199d8f3df1ec
Jervis Bay <jervis.bay@gmail.com>
2020-03-17 17:26:41 -0400

    added comments

--- test-script.js
+++ test-script.js
@@ -10,37 +10,34 @@ $(document).ready(function() {
             console.log(response);
             console.log(response.offices[4].officialIndices[0]);
 
+            // Define variables for senator index numbers and senator names and senator party
             var senatorSeniorNumber = response.offices[2].officialIndices[0];
             var senatorJuniorNumber = response.offices[2].officialIndices[1];
             var senatorSeniorName = response.officials[senatorSeniorNumber].name;
             var senatorJuniorName = response.officials[senatorJuniorNumber].name;
-
-
             var senatorSeniorParty = response.officials[senatorSeniorNumber].party;
             var senatorJuniorParty = response.officials[senatorJuniorNumber].party;
+
+            // Define variables for representative index numbers and representative names and senator party
             var representativeNumber = response.offices[3].officialIndices[0];
             var representativeName = response.officials[representativeNumber].name;
             var representativeParty = response.officials[representativeNumber].party;
 
+            // Define variables for governor index numbers and governor names and senator party
             var governorNumber = response.offices[4].officialIndices[0];
             var governorName = response.officials[governorNumber].name;
             var governorParty = response.officials[governorNumber].party;
 
+            // Create array to hold party affiliations
             var partyAffiliation = [senatorSeniorParty, senatorJuniorParty, representativeParty, governorParty];
 
+            // Console log checks
             console.log("Governor: " + governorName);
             console.log("Senior Senator: " + senatorSeniorName);
             console.log("Junior Senator: " + senatorJuniorName);
             console.log("Representative: " + representativeName);
 
-            console.log(senatorSeniorParty);
-            console.log(senatorJuniorParty);
-
-            console.log(representativeParty);
-            console.log(governorParty);
-            console.log(partyAffiliation);
-
-
+            // Create for loop to cycle through party affiliation array and count number of republicans / democrats / independents
             var i;
             for (i = 0; i < partyAffiliation.length; i++) {
                 if (partyAffiliation[i] === "Republican Party") {
@@ -56,6 +53,7 @@ $(document).ready(function() {
             }
 
         })
+
 
     // End of listener
 })
